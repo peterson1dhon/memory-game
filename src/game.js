fbf56c0fc007c8ea664d1f390241ad73ca9a655d
Peterson da Silva Caetano <98988530+peterson1dhon@users.noreply.github.com>
2022-09-04 13:39:31 -0300

    jogo finalizado

--- src/game.js
+++ src/game.js
@@ -1,4 +1,7 @@
 const grid = document.querySelector('.grid');
+const spanPlayer = document.querySelector('.player');
+const timer = document.querySelector('.timer');
+
 
 const characters = [
   'beth',
@@ -25,8 +28,10 @@ let secondCard = '';
 const checkEndGame = () => {
   const disabledCards = document.querySelectorAll('.disabled-card');
 
-  if (disabledCards.length == 20) {
-    alert('Parabéns, você conseguiu');
+  if (disabledCards.length === 20) {
+const spanPlayer = document.querySelector('.player');
+    clearInterval(this.loop);
+    alert(`Parabéns, ${spanPlayer.innerHTML}! Seu tempo foi: ${timer.innerHTML}`);
   }
 }
 
@@ -106,4 +111,17 @@ const loadGame = () => {
   });
 }
 
-loadGame();
+const startTimer = () => {
+
+  this.loop = setInterval(() => {
+    const currentTime = +timer.innerHTML;
+    timer.innerHTML = currentTime + 1;
+  }, 1000);
+
+}
+
+window.onload = () => {
+  spanPlayer.innerHTML = localStorage.getItem('player');
+  startTimer();
+  loadGame();
+}
